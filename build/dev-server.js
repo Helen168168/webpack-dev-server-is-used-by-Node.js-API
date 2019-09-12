43a012823ff47519de54bf374468927fe66fef50
pengxuemei <xuemei.peng@sipingsoft.com>
2019-09-12 10:24:39 +0800

    热加载

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,20 +1,17 @@
 const webpack = require('webpack');
 const config = require('../config')
-const path = require('path')
 const webpackDevServer = require('webpack-dev-server');
 const webpackConfig = require('./webpack.dev.conf');
-webpackConfig.entry.unshift("webpack-dev-server/client?http://localhost:8081/");
+webpackConfig.entry.unshift("webpack-dev-server/client?http://localhost:8081/", 'webpack/hot/dev-server');
 const options = {
   contentBase: config.dev.assetsPublicPath,
   hot: true,
-  inline:false,
+  inline: true,
   disableHostCheck: true,
   stats: { 
     colors: true 
   }
-
 }
-console.log(webpackConfig);
 webpackDevServer.addDevServerEntrypoints(webpackConfig, options);
 const compiler = webpack(webpackConfig);
 const server = new webpackDevServer(compiler, options);
